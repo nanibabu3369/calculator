27afc30bfb202f227ea09863ae24106444e0adf5
ramasubbaiah <ramasubbaiah@LAPTOP-8FTKGC45>
2022-12-07 11:09:32 +0530

    incresing code coverage

--- src/main/java/com/testingdocs/calculator/Calculator.java
+++ src/main/java/com/testingdocs/calculator/Calculator.java
@@ -24,12 +24,6 @@ public class Calculator {
 		return a/b;
 	}
 
-public static void main(String args[])
-{
-	Scanner sc=new Scanner(System.in);
-	int a=sc.nextInt();
-	int b=sc.nextInt();
-	
-}
+
 }
 	
